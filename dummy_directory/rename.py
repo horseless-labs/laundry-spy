41def22bc6b850ed2b3235bc4490ec190bae7400
Mireles <mireles@horseless.io>
2020-01-30 16:12:58 -0500

    Quick directory specification functionality to rename.py
    Former-commit-id: fb28fe65ed1a6f605b03cdffdd709549c1a41766
    Former-commit-id: 1f16f2f584ef88ae1da1e47f20f2d208db682d94

--- dummy_directory/rename.py
+++ dummy_directory/rename.py
@@ -1,16 +1,23 @@
 import re
 import os
 import random
+import argparse
 
 # Give every file in a directory a randomly-generated named
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--directory", required=False, help="Path to the directory to have its contents renamed", default="./")
+args = vars(ap.parse_args())
+
 def rename_all():
-    original_names = os.listdir()
+    f = args["directory"] + "/"
+    original_names = os.listdir(f)
 
     for name in original_names:
+        name = f + name
         _, extension = os.path.splitext(name)
-        new_name = r"%010x" % random.randint(0, 1000000000000) + extension
-        if re.search("\d+.*", name) is not None:
+        new_name = f + r"%010x" % random.randint(0, 1000000000000) + extension
+        if re.search(f + "\d+.*", name) is not None:
             os.rename(name, new_name)
 
 if __name__ == '__main__':
